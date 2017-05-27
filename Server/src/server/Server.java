ccd27a77ebf11cd41e04c600233036dfa544149f
Naqash Tanzeel <n.tanzeel@hotmail.co.uk>
2017-05-28 00:51:22 +0100

    Server - Cleaned Entry File and Removed Bin Folder.

--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -28,203 +28,164 @@ import server.world.ClanChatHandler;
 
 /**
  * Server.java
- * 
+ *
  * @author Sanity
  * @author Graham
  * @author Blake
  * @author Ryan Lmctruck30
- * 
  */
 
 public class Server {
 
-	public static boolean sleeping;
-	public static final int cycleRate;
-	public static boolean UpdateServer = false;
-	public static long lastMassSave = System.currentTimeMillis();
-	private static IoAcceptor acceptor;
-	private static ConnectionHandler connectionHandler;
-	private static ConnectionThrottleFilter throttleFilter;
-	private static SimpleTimer engineTimer, debugTimer;
-	private static long cycleTime, cycles, totalCycleTime, sleepTime;
-	private static DecimalFormat debugPercentFormat;
-	public static boolean shutdownServer = false;
-	public static int garbageCollectDelay = 40;
-	public static boolean shutdownClientHandler;
-	public static int serverlistenerPort;
-	public static ItemHandler itemHandler = new ItemHandler();
-	public static PlayerHandler playerHandler = new PlayerHandler();
-	public static NPCHandler npcHandler = new NPCHandler();
-	public static ShopHandler shopHandler = new ShopHandler();
-	public static ObjectHandler objectHandler = new ObjectHandler();
-	public static ObjectManager objectManager = new ObjectManager();
-	public static CastleWars castleWars = new CastleWars();
-	public static FightPits fightPits = new FightPits();
-	public static PestControl pestControl = new PestControl();
-	public static NPCDrops npcDrops = new NPCDrops();
-	public static ClanChatHandler clanChat = new ClanChatHandler();
-	public static FightCaves fightCaves = new FightCaves();
-	// public static WorldMap worldMap = new WorldMap();
-	// private static final WorkerThread engine = new WorkerThread();
+    private static final int cycleRate;
+    public static boolean UpdateServer = false;
+    private static long lastMassSave = System.currentTimeMillis();
+    private static SimpleTimer engineTimer, debugTimer;
+    private static long cycles;
+    private static long totalCycleTime;
+    private static long sleepTime;
+    private static DecimalFormat debugPercentFormat;
+    private static boolean shutdownServer = false;
+    private static int garbageCollectDelay = 40;
+    private static int serverlistenerPort;
+    public static ItemHandler itemHandler = new ItemHandler();
+    public static PlayerHandler playerHandler = new PlayerHandler();
+    public static NPCHandler npcHandler = new NPCHandler();
+    public static ShopHandler shopHandler = new ShopHandler();
+    public static ObjectHandler objectHandler = new ObjectHandler();
+    public static ObjectManager objectManager = new ObjectManager();
+    public static CastleWars castleWars = new CastleWars();
+    public static FightPits fightPits = new FightPits();
+    private static PestControl pestControl = new PestControl();
+    public static ClanChatHandler clanChat = new ClanChatHandler();
+    public static FightCaves fightCaves = new FightCaves();
 
-	static {
-		if (!Config.SERVER_DEBUG) {
-			serverlistenerPort = 43594;
-		} else {
-			serverlistenerPort = 43594;
-		}
-		cycleRate = 600;
-		shutdownServer = false;
-		engineTimer = new SimpleTimer();
-		debugTimer = new SimpleTimer();
-		sleepTime = 0;
-		debugPercentFormat = new DecimalFormat("0.0#%");
-	}
+    static {
+        if (!Config.SERVER_DEBUG) {
+            serverlistenerPort = 43594;
+        } else {
+            serverlistenerPort = 43594;
+        }
+        cycleRate = 600;
+        shutdownServer = false;
+        engineTimer = new SimpleTimer();
+        debugTimer = new SimpleTimer();
+        sleepTime = 0;
+        debugPercentFormat = new DecimalFormat("0.0#%");
+    }
 
-	// height,absX,absY,toAbsX,toAbsY,type
-	/*
-	 * public static final boolean checkPos(int height,int absX,int absY,int
-	 * toAbsX,int toAbsY,int type) { return
-	 * I.I(height,absX,absY,toAbsX,toAbsY,type); }
-	 */
-	public static void main(java.lang.String args[])
-			throws NullPointerException, IOException {
-		/**
-		 * Starting Up Server
-		 */
+    public static void main(java.lang.String args[])
+            throws NullPointerException, IOException {
+        /*
+          Starting Up Server
+         */
 
-		System.setOut(new Logger(System.out));
-		System.setErr(new Logger(System.err));
-		System.out.println("Launching Project Insanity...");
+        System.setOut(new Logger(System.out));
+        System.setErr(new Logger(System.err));
+        System.out.println("Launching Project Insanity...");
 
-		/**
-		 * World Map Loader
-		 */
-		// if(!Config.SERVER_DEBUG)
-		// VirtualWorld.init();
-		// WorldMap.loadWorldMap();
+        /*
+          Accepting Connections
+         */
+        IoAcceptor acceptor = new SocketAcceptor();
+        ConnectionHandler connectionHandler = new ConnectionHandler();
 
-		/**
-		 * Script Loader
-		 */
-		// ScriptManager.loadScripts();
+        SocketAcceptorConfig sac = new SocketAcceptorConfig();
+        sac.getSessionConfig().setTcpNoDelay(false);
+        sac.setReuseAddress(true);
+        sac.setBacklog(100);
 
-		/**
-		 * Accepting Connections
-		 */
-		acceptor = new SocketAcceptor();
-		connectionHandler = new ConnectionHandler();
+        ConnectionThrottleFilter throttleFilter = new ConnectionThrottleFilter(Config.CONNECTION_DELAY);
+        sac.getFilterChain().addFirst("throttleFilter", throttleFilter);
+        acceptor.bind(new InetSocketAddress(serverlistenerPort),
+                connectionHandler, sac);
 
-		SocketAcceptorConfig sac = new SocketAcceptorConfig();
-		sac.getSessionConfig().setTcpNoDelay(false);
-		sac.setReuseAddress(true);
-		sac.setBacklog(100);
+        /*
+          Initialise Handlers
+         */
+        EventManager.initialize();
+        Connection.initialize();
 
-		throttleFilter = new ConnectionThrottleFilter(Config.CONNECTION_DELAY);
-		sac.getFilterChain().addFirst("throttleFilter", throttleFilter);
-		acceptor.bind(new InetSocketAddress(serverlistenerPort),
-				connectionHandler, sac);
+        /*
+          Server Successfully Loaded
+         */
+        System.out.println("Server listening on port 0.0.0.0:" + serverlistenerPort);
+        /*
+          Main Server Tick
+         */
+        try {
+            while (!Server.shutdownServer) {
+                if (sleepTime >= 0)
+                    Thread.sleep(sleepTime);
+                else
+                    Thread.sleep(600);
+                engineTimer.reset();
+                itemHandler.process();
+                playerHandler.process();
+                npcHandler.process();
+                shopHandler.process();
+                objectManager.process();
+                fightPits.process();
+                pestControl.process();
+                long cycleTime = engineTimer.elapsed();
+                sleepTime = cycleRate - cycleTime;
+                totalCycleTime += cycleTime;
+                cycles++;
+                debug();
+                garbageCollectDelay--;
+                if (garbageCollectDelay == 0) {
+                    garbageCollectDelay = 40;
+                    System.gc();
+                }
+                if (System.currentTimeMillis() - lastMassSave > 300000) {
+                    for (Player p : PlayerHandler.players) {
+                        if (p == null)
+                            continue;
+                        PlayerSave.saveGame((Client) p);
+                        System.out.println("Saved game for " + p.playerName
+                                + ".");
+                        lastMassSave = System.currentTimeMillis();
+                    }
 
-		/**
-		 * Initialise Handlers
-		 */
-		EventManager.initialize();
-		Connection.initialize();
-		// PlayerSaving.initialize();
-		// MysqlManager.createConnection();
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.out.println("A fatal exception has been thrown!");
+            for (Player p : PlayerHandler.players) {
+                if (p == null)
+                    continue;
+                PlayerSave.saveGame((Client) p);
+                System.out.println("Saved game for " + p.playerName + ".");
+            }
+        }
+        System.exit(0);
+    }
 
-		/**
-		 * Server Successfully Loaded
-		 */
-		System.out.println("Server listening on port 0.0.0.0:"
-				+ serverlistenerPort);
-		/**
-		 * Main Server Tick
-		 */
-		try {
-			while (!Server.shutdownServer) {
-				if (sleepTime >= 0)
-					Thread.sleep(sleepTime);
-				else
-					Thread.sleep(600);
-				engineTimer.reset();
-				itemHandler.process();
-				playerHandler.process();
-				npcHandler.process();
-				shopHandler.process();
-				objectManager.process();
-				fightPits.process();
-				pestControl.process();
-				cycleTime = engineTimer.elapsed();
-				sleepTime = cycleRate - cycleTime;
-				totalCycleTime += cycleTime;
-				cycles++;
-				debug();
-				garbageCollectDelay--;
-				if (garbageCollectDelay == 0) {
-					garbageCollectDelay = 40;
-					System.gc();
-				}
-				if (System.currentTimeMillis() - lastMassSave > 300000) {
-					for (Player p : PlayerHandler.players) {
-						if (p == null)
-							continue;
-						PlayerSave.saveGame((Client) p);
-						System.out.println("Saved game for " + p.playerName
-								+ ".");
-						lastMassSave = System.currentTimeMillis();
-					}
 
-				}
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			System.out.println("A fatal exception has been thrown!");
-			for (Player p : PlayerHandler.players) {
-				if (p == null)
-					continue;
-				PlayerSave.saveGame((Client) p);
-				System.out.println("Saved game for " + p.playerName + ".");
-			}
-		}
-		acceptor = null;
-		connectionHandler = null;
-		sac = null;
-		System.exit(0);
-	}
+    private static boolean playerExecuted = false;
 
-	public static void processAllPackets() {
-		for (int j = 0; j < PlayerHandler.players.length; j++) {
-			if (PlayerHandler.players[j] != null) {
-				while (PlayerHandler.players[j].processQueuedPackets())
-					;
-			}
-		}
-	}
+    private static void debug() {
+        if (debugTimer.elapsed() > 360 * 1000 || playerExecuted) {
+            long averageCycleTime = totalCycleTime / cycles;
+            System.out
+                    .println("Average Cycle Time: " + averageCycleTime + "ms");
+            double engineLoad = ((double) averageCycleTime / (double) cycleRate);
+            System.out
+                    .println("Players online: " + PlayerHandler.playerCount
+                            + ", engine load: "
+                            + debugPercentFormat.format(engineLoad));
+            totalCycleTime = 0;
+            cycles = 0;
+            System.gc();
+            System.runFinalization();
+            debugTimer.reset();
+            playerExecuted = false;
+        }
+    }
 
-	public static boolean playerExecuted = false;
-
-	private static void debug() {
-		if (debugTimer.elapsed() > 360 * 1000 || playerExecuted) {
-			long averageCycleTime = totalCycleTime / cycles;
-			System.out
-					.println("Average Cycle Time: " + averageCycleTime + "ms");
-			double engineLoad = ((double) averageCycleTime / (double) cycleRate);
-			System.out
-					.println("Players online: " + PlayerHandler.playerCount
-							+ ", engine load: "
-							+ debugPercentFormat.format(engineLoad));
-			totalCycleTime = 0;
-			cycles = 0;
-			System.gc();
-			System.runFinalization();
-			debugTimer.reset();
-			playerExecuted = false;
-		}
-	}
-
-	public static long getSleepTimer() {
-		return sleepTime;
-	}
+    public static long getSleepTimer() {
+        return sleepTime;
+    }
 
 }
