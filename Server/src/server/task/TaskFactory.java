206d35b66fca2ad764eb76ca248129349fb0611c
Naqash Tanzeel <n.tanzeel@hotmail.co.uk>
2017-05-28 01:48:08 +0100

    Server - Removed MysqlManager, ScriptManager and ShutDownHook.

--- Server/src/server/task/TaskFactory.java
+++ Server/src/server/task/TaskFactory.java
@@ -1,53 +1,9 @@
 package server.task;
 
 import server.model.players.Player;
-import server.model.players.PlayerHandler;
-import server.util.ScriptManager;
 
 public class TaskFactory {
 
-	public static Task getDelayedTask(final String callbackFunction,
-                                      final Player player, final int actionX, final int actionY) {
-		Task task = new Task(player, false) {
-			@Override
-			public void run() {
-				synchronized (PlayerHandler.players) {
-					ScriptManager.callFunc(callbackFunction, player, actionX,
-							actionY);
-				}
-			}
-		};
-		return task;
-	}
-
-	public static Task getDelayedTask(final String callbackFunction,
-                                      final Player player, final int actionID, final int actionX,
-                                      final int actionY) {
-		Task task = new Task(player, false) {
-			@Override
-			public void run() {
-				synchronized (PlayerHandler.players) {
-					ScriptManager.callFunc(callbackFunction, player, actionID,
-							actionX, actionY);
-				}
-			}
-		};
-		return task;
-	}
-
-	public static Task getDelayedGlobalTask(final String callbackFunction,
-			final int actionID, final int actionX, final int actionY) {
-		Task task = new Task(null, true) {
-			@Override
-			public void run() {
-				synchronized (PlayerHandler.players) {
-					ScriptManager.callFunc(callbackFunction, actionID, actionX,
-							actionY);
-				}
-			}
-		};
-		return task;
-	}
 
 	protected static class Task implements Runnable {
 
